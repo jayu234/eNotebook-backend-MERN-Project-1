6eec15a44a3db75e1f295452e803d60561931c28
Jayu <jaivikgoswami780@gmail.com>
2022-02-07 12:08:06 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,10 +1,6 @@
 const mongoose = require('mongoose');
-<<<<<<< HEAD
 import dotenv from 'dotenv';
 const mongoURI = process.env.MONGOURI;
-=======
-const mongoURI = "mongodb+srv://jayu23:Jaivik2304@enotebook.hhrgb.mongodb.net/test"
->>>>>>> 988538aafb2fc68d2ad099d40870228a55dbc3a7
 
 dotenv.config();
 const connectToMongo = ()=>{
