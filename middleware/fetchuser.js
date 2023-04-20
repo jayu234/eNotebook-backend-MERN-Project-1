c9e7552ee320573f23d252560802aa0b40e60731
Jayu <jaivikgoswami780@gmail.com>
2023-04-20 14:23:35 +0530

    minor bug fixes

--- middleware/fetchuser.js
+++ middleware/fetchuser.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken');
-const JWT_SWCRET = process.env.JWT_SWCRET;
+const JWT_SWCRET = process.env.JWT_SECRET;
 const fetchuser = (req, res, next)=>{
 
     const token = req.header('auth-token');
